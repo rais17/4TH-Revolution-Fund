b174eec43d65ee669ea693cfad1d0d55879da3c3
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-12-03 03:00:23 +0530

    Update TechCard.js

--- src/Components/TechCard.js
+++ src/Components/TechCard.js
@@ -13,14 +13,14 @@ const TechCard = ({ title, description, image, reverse, id }) => {
   return (
     <div
       className={`flex ${
-        reverse ? `flex-row-reverse` : `flex-row pl-4`
-      } max-w-6xl w-full at460:w-11/12  shadow-xl py-8 gap-x-8  relative xxs:max-at602:h-[60vh] at602:max-at830:h-[55vh]`}
+        reverse ? `at830:flex-row-reverse` : `at830:flex-row at830:pl-4`
+      } max-w-6xl w-full at460:w-11/12  shadow-xl py-8 gap-x-8  relative`}
     >
-      <div className="w-full h-full space-y-7 sm:space-y-8 xxs:max-at830:absolute xxs:max-at830:inset-0 xxs:max-at830:z-20 xxs:max-at830:flex xxs:max-at830:flex-col xxs:max-at830:items-center at830:w-2/3 xxs:max-sm:pt-8 xxs:max-sm:px-4 sm:max-at830:p-8">
+      <div className="w-full h-full space-y-7 sm:space-y-8 xxs:max-at830:relative xxs:max-at830:inset-0 xxs:max-at830:z-20 xxs:max-at830:flex xxs:max-at830:flex-col xxs:max-at830:items-center at830:w-2/3 xxs:max-sm:px-2 sm:max-at830:p-8">
         <h1 className="text-4xl font-medium text-gray-100 font-Titillium xxs:max-at830:text-center">
           {title}
         </h1>
-        <p className="text-2xl font-medium text-gray-300 at830:text-xl xxs:max-at830:text-center font-Titillium">
+        <p className="px-2 text-2xl font-medium text-gray-300 at830:text-xl xxs:max-at830:text-center font-Titillium">
           {description}
         </p>
         <button
@@ -39,7 +39,7 @@ const TechCard = ({ title, description, image, reverse, id }) => {
         />
         <div className="absolute inset-0 bg-black opacity-70 at830:hidden"></div>
       </div>
-    </div>
+    </div>    
   );
 };
 
