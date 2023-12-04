2b7c9f875d6a89096bf652c33427a732914bf71f
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-12-05 00:39:35 +0530

    Update Quotes.js

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -8,14 +8,14 @@ const Quotes = ({description, image, isReverse}) => {
       } items-center gap-10 px-4 sm:px-6 shadow-2xl relative overflow-hidden`}
     >
       <div className="inset-0 z-20 sm:w-2/3 xxs:max-sm:relative">
-        <pre className="text-xl text-gray-300 whitespace-pre-line sm:text-justify font-Titillium xxs:max-sm:py-8 ">
+        <pre className="text-xl text-gray-200 whitespace-pre-line sm:text-justify font-Titillium xxs:max-sm:py-8 ">
           {description}
         </pre>
       </div>
 
       <div className="relative inset-0 z-10 overflow-hidden sm:w-1/3 rounded-2xl xxs:max-sm:absolute">
         <img className="object-cover w-full h-full" src={image} alt="img" />
-        <div className="absolute inset-0 bg-black opacity-70 sm:hidden"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-60 sm:hidden"></div>
       </div>
     </div>
   );
